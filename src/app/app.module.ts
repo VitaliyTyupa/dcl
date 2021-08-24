c538f794d95a47cd4d2a20daf33383f2a3d4fae8
Vit Tiupa <vitaliy.t@zazmic.com>
2021-08-24 13:56:19 +0300

    Added component loader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponent } from './main/main.component';
+import { RedComponent } from './main/red/red.component';
+import { GreenComponent } from './main/green/green.component';
+import { BlueComponent } from './main/blue/blue.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { MainContentComponent } from './main/main-content/main-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    RedComponent,
+    GreenComponent,
+    BlueComponent,
+    MainContentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
